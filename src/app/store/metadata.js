aebad77d1c3fb19c1c477665230f36095b5ff2f1
laihj <hamainter@gmail.com>
2025-11-17 11:19:49 +0800

    fixed vecel issue

---
+++ src/app/store/metadata.js
@@ -0,0 +1,24 @@
+export const metadata = {
+  title: "PaceGuru - Your Running Companion",
+  description: "记录跑步数据，制定训练计划，突破个人最佳成绩",
+  openGraph: {
+    title: "PaceGuru",
+    description: "记录跑步数据，制定训练计划，突破个人最佳成绩",
+    url: 'https://paceguru.app/store',
+    type: 'website',
+    images: [
+      {
+        url: '/ograph.png',
+        width: 1200,
+        height: 630,
+        alt: 'PaceGuru - Running App',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "PaceGuru",
+    description: "记录跑步数据，制定训练计划，突破个人最佳成绩",
+    images: ['/ograph.png'],
+  },
+};
